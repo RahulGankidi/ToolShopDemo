14c91e5e83391569936a02f9829f93b7926690a3
RahulGankidi <gankidirahul123@gmail.com>
2025-01-03 10:54:26 +0000

    added extent report code

--- src/main/java/com/toolshop/pages/BasePage.java
+++ src/main/java/com/toolshop/pages/BasePage.java
@@ -5,19 +5,34 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class BasePage {
 	
 	public WebDriver driver;
+	 public static ExtentReports extent;
+	    public static ExtentTest test;
 	
 	@BeforeSuite
 	public void openBrowserUrl() {
+		ExtentSparkReporter sparkReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+        
+        test = extent.createTest("Base Test Setup").info("Test Suite Execution Started");
+        
 		driver = new EdgeDriver();
 		driver.get("https://practicesoftwaretesting.com/");
 		driver.manage().window().maximize();
+		test.pass("Browser launched and URL opened successfully.");
 	}
 	
 	@AfterSuite
 	public void closeBrowserUrl() {
+		extent.flush();
+		test.pass("Browser closed successfully.");
 		driver.quit();
 	}
 	
